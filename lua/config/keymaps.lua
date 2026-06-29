f91a7dad1afeba8416b43ee7565cb483a607fecc
Jared Alonzo <jared.alonzo@icloud.com>
2026-06-29 00:48:35 -0400

    snacks terminal <C-q> keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Terminal toggle for snacks
+vim.keymap.set({ "n", "t" }, "<C-q>", function()
+  Snacks.terminal.toggle(nil, { cwd = LazyVim.root() })
+end, { desc = "Toggle Terminal (root)" })
