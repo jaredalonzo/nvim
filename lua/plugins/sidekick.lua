61662dd36bc5af075deee5701d446b55ca37850b
Jared Alonzo <jared.alonzo@icloud.com>
2026-07-17 13:33:12 -0400

    add sidekick.lua floating window

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    cli = {
+      win = {
+        layout = "float",
+        float = {
+          border = "rounded",
+          width = 0.7,
+          height = 0.7,
+        },
+      },
+    },
+  },
+}
