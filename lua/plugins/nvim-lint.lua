85675a5ea1ae13936726539fbd6abb2016e9fbcc
Jared Alonzo <jared.alonzo@icloud.com>
2026-07-20 13:21:20 -0400

    add python linting config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,6 +1,10 @@
 return {
   "mfussenegger/nvim-lint",
   opts = {
+    linters_by_ft = {
+      markdown = { "markdownlint-cli2" },
+      python = { "mypy", "ruff" },
+    },
     linters = {
       ["markdownlint-cli2"] = {
         args = { "--config", vim.fn.expand("~/.config/nvim/markdownlint.jsonc"), "--" },
