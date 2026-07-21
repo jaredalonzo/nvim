f1437b1c30e184e9c15543c60f26e26bec2c4a2a
Jared Alonzo <jared.alonzo@icloud.com>
2026-07-21 16:55:35 -0400

    change formatter plugin to ruff

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "black", "isort" },
+      python = { "ruff_format", "ruff_organize_imports" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   },
