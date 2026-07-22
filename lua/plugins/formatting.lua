3974fe01a72b49ac995161bf98cd4866e9737705
Jared Alonzo <jared.alonzo@icloud.com>
2026-07-22 14:09:51 -0400

    add formatters for js/jsx and ts/tsx

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,7 +4,19 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "ruff_format", "ruff_organize_imports" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
+      -- prefer dprint when the project configures it, else fall back to prettier
+      javascript = { "dprint", "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "dprint", "prettierd", "prettier", stop_after_first = true },
+      typescript = { "dprint", "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "dprint", "prettierd", "prettier", stop_after_first = true },
+    },
+    formatters = {
+      dprint = {
+        -- only run dprint in projects that actually configure it
+        condition = function(_, ctx)
+          return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1] ~= nil
+        end,
+      },
     },
   },
 }
