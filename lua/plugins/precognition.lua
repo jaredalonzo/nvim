922e8d1240f0627bc32f02136db0c36a01b7fcb0
Jared Alonzo <jared.alonzo@icloud.com>
2026-08-03 11:44:24 -0400

    add config to render virtual lines above cursor

--- lua/plugins/precognition.lua
+++ lua/plugins/precognition.lua
@@ -5,7 +5,12 @@ return {
     -- show hints as soon as you enter a buffer (toggle off if it feels noisy)
     startVisible = true,
     showBlankVirtLine = true,
-    highlightColor = { link = "Comment" },
+    highlightFullVirtLine = true, -- pad the hint line to full width so the bg shows
+    -- comment-colored hints on a subtle highlighted line (tokyonight values; tweak to taste)
+    highlightColor = { fg = "#565f89", bg = "#292e42" },
+    targetedMotionHints = {
+      enabled = false,
+    },
     hints = {
       Caret = { text = "^", prio = 2 },
       Dollar = { text = "$", prio = 1 },
@@ -20,6 +25,33 @@ return {
     },
   },
   keys = {
-    { "<leader>uP", function() require("precognition").toggle() end, desc = "Toggle Precognition" },
+    {
+      "<leader>uP",
+      function()
+        require("precognition").toggle()
+      end,
+      desc = "Toggle Precognition",
+    },
   },
+  config = function(_, opts)
+    require("precognition").setup(opts)
+
+    -- Render the hint line ABOVE the cursor so it doesn't punch a gap
+    -- through tiny-inline-diagnostic's multiline overlay below the cursor.
+    local mt = getmetatable(require("precognition.renderer").new())
+    mt.render_inline_hint_virt_line = function(self, bufnr, cursorline, virt_line)
+      if self._extmark and self._extmark_buf ~= bufnr then
+        if self._extmark_buf and vim.api.nvim_buf_is_valid(self._extmark_buf) then
+          vim.api.nvim_buf_del_extmark(self._extmark_buf, self._ns, self._extmark)
+        end
+        self._extmark, self._extmark_buf = nil, nil
+      end
+      self._extmark = vim.api.nvim_buf_set_extmark(bufnr, self._ns, cursorline - 1, 0, {
+        id = self._extmark,
+        virt_lines = { virt_line },
+        virt_lines_above = true, -- render above the cursor line
+      })
+      self._extmark_buf = bufnr
+    end
+  end,
 }
